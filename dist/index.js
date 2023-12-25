e025945d9e7926406214abe9510f2f21b9c6299c
Aryan Karamtoth <88923986+SpaciousCoder78@users.noreply.github.com>
2023-12-25 09:00:39 +0530

    changed name

--- dist/index.js
+++ dist/index.js
@@ -2,5 +2,5 @@ console.log("hello world");
 document.querySelector('#app').innerHTML = `
   <div style="background-color: darkblue; color: white; padding: 20px;">
     
-    <h1>Microsoft Learn Student Community KITSW</h1>
+    <h1>Segmentation Foxes</h1>
   </div>`;
