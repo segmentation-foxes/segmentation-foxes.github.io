6704c6779ebd9b9f4d98d950d126d2d74fb2a337
Aryan Karamtoth <88923986+SpaciousCoder78@users.noreply.github.com>
2023-12-25 08:54:28 +0530

    initial commit

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+console.log("hello world");
+document.querySelector('#app').innerHTML = `
+  <div style="background-color: darkblue; color: white; padding: 20px;">
+    
+    <h1>Microsoft Learn Student Community KITSW</h1>
+  </div>`;
